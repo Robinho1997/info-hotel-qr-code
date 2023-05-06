426bbcbab0266d627358c292ddb3255d724e367e
Robinho1997 <robin.kempen@gmx.de>
2023-05-06 23:46:11 +0200

    toggle language

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const toggleRestaurantsBtn = document.getElementById("toggle-restaurants-btn");
-const toggleAusflugszieleBtn = document.getElementById("toggle-ausflugsziele-btn")
-const toggleIconRestaurant = document.getElementById("toggle-icon-restaurants")
-const toggleIconAusflugsziele = document.getElementById("toggle-icon-ausflugsziele")
+const toggleIconRestaurant = document.getElementById("toggle-icon-restaurants");
 const restaurantList = document.getElementById("restaurant-list");
-const ausflugsZieleList = document.getElementById("ausflugsziele-list")
+const toggleAusflugszieleBtn = document.getElementById(
+  "toggle-ausflugsziele-btn"
+);
+const toggleIconAusflugsziele = document.getElementById(
+  "toggle-icon-ausflugsziele"
+);
+const ausflugsZieleList = document.getElementById("ausflugsziele-list");
 
-toggleRestaurantsBtn.addEventListener("click",()=>toggleRestaurants(restaurantList,toggleIconRestaurant))
-toggleAusflugszieleBtn.addEventListener("click",()=>toggleRestaurants(ausflugsZieleList,toggleIconAusflugsziele))
+const switchLanguageBtn = document.getElementById("switch-language-btn");
+const langGerman = document.querySelectorAll('[lang="de"]');
+const langEnglish = document.querySelectorAll('[lang="en"]');
+let isGerman = false;
 
-function toggleRestaurants(list,icon) {
-    if (list.style.display === "none") {
-        list.style.display = "block"
-        icon.innerHTML = "expand_less"
+
+
+toggleRestaurantsBtn.addEventListener("click", () =>
+  toggleRestaurants(restaurantList, toggleIconRestaurant)
+);
+toggleAusflugszieleBtn.addEventListener("click", () =>
+  toggleRestaurants(ausflugsZieleList, toggleIconAusflugsziele)
+);
+switchLanguageBtn.addEventListener("click", toggleLanguage)
+
+function toggleRestaurants(list, icon) {
+  if (list.style.display === "none") {
+    list.style.display = "block";
+    icon.innerHTML = "expand_less";
+  } else {
+    list.style.display = "none";
+    icon.innerHTML = "expand_more";
+  }
+}
+toggleLanguage()
+function toggleLanguage() {
+    if(isGerman) {
+        langGerman.forEach(el => el.style.display = "none");
+        langEnglish.forEach(el => el.style.display = "inline");
+        document.querySelector("html").style.display = "block"
+        isGerman = false;
     } else {
-        list.style.display = "none"
-        icon.innerHTML = "expand_more"
+        langGerman.forEach(el => el.style.display = "inline");
+        langEnglish.forEach(el => el.style.display = "none");
+        document.querySelector("html").style.display = "block"
+        isGerman = true;
     }
 }
